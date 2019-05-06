843ad08a20e358877a765943fe86d9f2c64111b9
SomaUrushidani <soma@SomaUrushidaninoMacBook-Pro.local>
2019-05-06 13:49:43 +0900

    done lint

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,27 @@
 <template>
-    <div>
-  <b-navbar class="header-color" toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand to="/">
-    Project SoMA
-    </b-navbar-brand>
+  <div>
+    <b-navbar class="header-color" toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand to="/">
+        Project SoMA
+      </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse" />
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item to="/articles" :class="{ active:articlesisActive }">Articles</b-nav-item>
-        <b-nav-item to="/about" :class="{ active:aboutisActive }">About</b-nav-item>
-        <b-nav-item to="/contact" :class="{ active:contactisActive }">Contact</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/articles" :class="{ active:articlesisActive }">
+            Articles
+          </b-nav-item>
+          <b-nav-item to="/about" :class="{ active:aboutisActive }">
+            About
+          </b-nav-item>
+          <b-nav-item to="/contact" :class="{ active:contactisActive }">
+            Contact
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
