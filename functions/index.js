f28f896e82a515ff90c368215643e6dbbe9b81d6
kkiriyama <n.kurata1005@gmail.com>
2019-05-08 02:06:49 +0900

    implement SSR

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable promise/always-return */
 const functions = require('firebase-functions');
 const express = require('express');
 const { Nuxt } = require('nuxt');
@@ -12,12 +13,54 @@ const nuxt = new Nuxt({
     }
 });
 
-async function handleRequest(req, res) {
-    res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
-    await nuxt.ready()
-    return await nuxt.render(req, res)
-}
+app.get('/', (req, res) => {
+  nuxt.renderRoute('/', { req })
+    .then((result) => {
+      res.send(result.html)
+    })
+    .catch((e) => {
+      res.send(e)
+    })
+})
 
-app.use(handleRequest);
+app.get('/about', (req, res) => {
+  nuxt.renderRoute('/about', { req })
+    .then((result) => {
+      res.send(result.html)
+    })
+    .catch((e) => {
+      res.send(e)
+    })
+})
+
+app.get('/contact', (req, res) => {
+  nuxt.renderRoute('/contact', { req })
+    .then((result) => {
+      res.send(result.html)
+    })
+    .catch((e) => {
+      res.send(e)
+    })
+})
+
+app.get('/articles', (req, res) => {
+  nuxt.renderRoute('/articles', { req })
+    .then((result) => {
+      res.send(result.html)
+    })
+    .catch((e) => {
+      res.send(e)
+    })
+})
+
+app.get('/articles/:slug', (req, res) => {
+  nuxt.renderRoute('/articles/' + req.params.slug, { req })
+    .then((result) => {
+      res.send(result.html)
+    })
+    .catch((e) => {
+      res.send(e)
+    })
+})
 
 exports.ssrapp = functions.https.onRequest(app);
