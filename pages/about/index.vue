5ddafeebe2706b003b347f543f9e8e03c8766612
Lenqth <nullptr00@gmail.com>
2019-05-19 13:36:41 +0900

    aboutの反映忘れ

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -55,5 +55,10 @@ export default {
 #about-page-writers{
   margin-top: 4rem;
 }
+.card-deck {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 0.5rem
+}
 
 </style>
