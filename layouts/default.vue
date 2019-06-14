3687bdab0ce7e3c10f86c4f0060a9bc00650d71a
akihiro iwata <akihiro.iwata.1990@gmail.com>
2019-06-15 01:16:44 +0900

    vueの仕様上、scopedがないと全コンポーネントに当該styleが適用されるため、これを防ぐ

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,6 +32,15 @@ html {
   margin: 0;
 }
 
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
 .button--green {
   display: inline-block;
   border-radius: 4px;
