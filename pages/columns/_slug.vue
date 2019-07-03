a6a18d4fe647568913731543da0ea1c51025b835
kkiriyama <n.kurata1005@gmail.com>
2019-07-04 06:08:02 +0900

    column詳細で画像が表示されないバグを修正

--- pages/columns/_slug.vue
+++ pages/columns/_slug.vue
@@ -94,17 +94,12 @@ export default {
         this.favoriteArticle()
       }
     },
-    renderImage(file) {
-      return `<img src=${file.url}/>`
-    },
     renderedDocument() {
       const options = {
         renderNode: {
           'embedded-asset-block': (node) => {
             const file = node.data.target.fields.file
-            const jsx = this.renderImage(file)
-            if (!jsx.data) return ''
-            return '<div align="center"><img src=' + jsx.data.attrs.src + ' style="max-width: 80%;"></div>'
+            return '<div align="center"><img src=' + file.url + ' style="max-width: 80%;"></div>'
           }
         }
       }
